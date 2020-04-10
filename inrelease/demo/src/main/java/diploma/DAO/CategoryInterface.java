f8eecfecd04d5b713b2a81a63d1a34acf431c8db
feuwie <56365722+feuwie@users.noreply.github.com>
2020-04-10 17:14:12 +0300

    s_change

--- inrelease/demo/src/main/java/diploma/DAO/CategoryInterface.java
+++
@@ -1,9 +0,0 @@
-package diploma.DAO;
-
-import java.util.List;
-import javax.sql.DataSource;
-
-public interface CategoryInterface<T> {
-   public void setDataSource(DataSource ds);
-   public List<T> getAll();
-}
